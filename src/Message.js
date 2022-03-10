6688d99c3c646fa0899ab74f36d6a01b98baf551
ivan-radchenko <ivan.msk95@gmail.com>
2022-03-10 19:42:06 +0300

    lesson 2

---
+++ src/Message.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Message = ({ text }) => {
+    return (
+        <div >
+            <h1 >Hello {text}!</h1>
+        </div >
+    );
+}
+
+export default Message;
